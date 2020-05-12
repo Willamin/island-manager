8dbe104c5d02ae17adad118676240d9db2c52eb9
Will Lewis <will@wflewis.com>
2020-05-11 23:00:07 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/root'
-  get 'pages/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#root'
+  get '/about', to: 'pages#about'
 end
