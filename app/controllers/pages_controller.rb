67c88b4045779b708ca695495a0e44ba7c2d43de
Will Lewis <will@wflewis.com>
2020-05-13 07:28:24 -0400

    Return from the secret route to avoid a bogus query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,7 @@ class PagesController < ApplicationController
       flash[:alert] = "You must be signed in to view this page"
       session[:user_return_to] = "/secret"
       redirect_to :root
+      return
     end
 
     @user = User.find(current_user["id"])
